fd98bc06097e00fb89066cc0049a3d31271e0188
Mark Nagy <mark.nagy@prezi.com>
2018-11-26 22:04:32 +0100

    Initial Commit

---
+++ sillywalksaver/sillywalksaverView.h
@@ -0,0 +1,13 @@
+//
+//  sillywalksaverView.h
+//  sillywalksaver
+//
+//  Created by Mark Nagy on 2018. 11. 26..
+//  Copyright © 2018. Mark Nagy. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface sillywalksaverView : ScreenSaverView
+
+@end
---
+++ sillywalksaver/sillywalksaverView.m
@@ -0,0 +1,52 @@
+//
+//  sillywalksaverView.m
+//  sillywalksaver
+//
+//  Created by Mark Nagy on 2018. 11. 26..
+//  Copyright © 2018. Mark Nagy. All rights reserved.
+//
+
+#import "sillywalksaverView.h"
+
+@implementation sillywalksaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
